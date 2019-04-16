f7d93e736c2e128f7ecfd8deb8be810cb61cf4ce
Yosep Kim <yosepk@gmail.com>
2019-04-15 23:42:00 -0400

    Test

--- cicd/tasks/update-deployment.sh
+++ cicd/tasks/update-deployment.sh
@@ -8,8 +8,10 @@ VERSION=$(cat bump/version)
 
 RESULT=$(sed -e "s|${IMAGE_NAME}:.*|${IMAGE_NAME}:${VERSION}|g" source/pks/deployment.yml)
 
-echo $RESULT > deployment-new.yml
+echo $RESULT > deployment.yml
 
-cat source/pks/depoyment-new.yml
+#cat source/pks/depoyment-new.yml
 
-mv source/pks/deployment-new.yml source/pks/deployment.yml
+cat deployment.yml
+
+#mv source/pks/deployment-new.yml source/pks/deployment.yml
