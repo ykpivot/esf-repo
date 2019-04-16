02f520954d7d24032444793b193f26bf469f89f8
Yosep Kim <yosepk@gmail.com>
2019-04-16 00:04:27 -0400

    test

--- cicd/tasks/update-deployment.sh
+++ cicd/tasks/update-deployment.sh
@@ -4,17 +4,16 @@ set -ex
 
 echo "Updating deployment"
 
+git clone source out
+
 VERSION=$(cat bump/version)
 
-sed -e "s|${IMAGE_NAME}:.*|${IMAGE_NAME}:${VERSION}|g" source/pks/deployment.yml > out/deployment.yml
+sed -e "s|${IMAGE_NAME}:.*|${IMAGE_NAME}:${VERSION}|g" source/pks/deployment.yml > out/pks/deployment.yml
 
 cd out
-ls
 
 git config --global user.email "esf@ci.cd"
 git config --global user.name "esf cicd"
 
 git add .
 git commit -m "Updates by Concourse"
-
-exit 0
