dc989caf31ff28b3714d092467929a60766c884b
Yosep Kim <yosepk@gmail.com>
2019-04-15 23:56:05 -0400

    test

--- cicd/tasks/update-deployment.sh
+++ cicd/tasks/update-deployment.sh
@@ -9,6 +9,7 @@ VERSION=$(cat bump/version)
 sed -e "s|${IMAGE_NAME}:.*|${IMAGE_NAME}:${VERSION}|g" source/pks/deployment.yml > out/deployment.yml
 
 cd out
+ls
 
 git config --global user.email "esf@ci.cd"
 git config --global user.name "esf cicd"
