f9e93b2a9427e84a65527425b47ffc3080cbb5fd
Yosep Kim <yosepk@gmail.com>
2019-04-16 00:44:03 -0400

    files

--- cicd/tasks/provision-cluster.sh
+++ cicd/tasks/provision-cluster.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+set +ex
+
 echo "Provisioning the cluster"
+
+pks
+
+kubectl
+
+pks login -a $PKS_URL -u $PKS_USER -p $PKS_PASSWORD --skip-ssl-validation
+
+pks get-credentials $PKS_CLUSTER
+
+kubectl apply -f source/pks/deployment.yml
