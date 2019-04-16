bf6a1a0d93b50240836d36d43b56a002d37f7661
Yosep Kim <yosepk@gmail.com>
2019-04-15 23:25:17 -0400

    Clean up regex

--- cicd/tasks/update-deployment.sh
+++ cicd/tasks/update-deployment.sh
@@ -6,7 +6,7 @@ echo "Updating deployment"
 
 VERSION=$(cat bump/version)
 
-sed -i -e 's/$IMAGE_NAME:.*/$IMAGE_NAME:$VERSION/g' source/pks/deployment.yml > source/pks/deployment-new.yml
+sed -e "s/${IMAGE_NAME}:.*/${IMAGE_NAME}:${VERSION}/g" source/pks/deployment.yml > source/pks/deployment-new.yml
 
 cat source/pks/depoyment-new.yml
 
