25018c8bc5f62df7a30802da07300f2da794c55e
Yosep Kim <yosepk@gmail.com>
2019-04-24 10:01:50 -0400

    Create secret

--- cicd/tasks/provision-cluster.sh
+++ cicd/tasks/provision-cluster.sh
@@ -8,4 +8,6 @@ echo "Provisioning the cluster"
 
 pwsh /usr/local/bin/get-pks-k8s-config.ps1 $PKS_URL $PKS_CLUSTER $PKS_USER $PKS_PASSWORD
 
+kubectl create secret generic esf-secret --from-file=deployment-source/secret/connection-string=connection.xml
+
 kubectl apply -f deployment-source/pks/deployment.yml
